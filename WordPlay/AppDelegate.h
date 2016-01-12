46df0888ff988d8179a809f5eca0e4e4b18dfba5
Matt Deuschle <mdeuschle@gmail.com>
2016-01-11 19:09:33 -0600

    Initial Commit

---
+++ WordPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlay
+//
+//  Created by Matt Deuschle on 1/11/16.
+//  Copyright © 2016 Matt Deuschle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordPlay
+//
+//  Created by Matt Deuschle on 1/11/16.
+//  Copyright © 2016 Matt Deuschle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
